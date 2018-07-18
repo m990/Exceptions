08a731619ce3a8cc8525a0850a07b36b5b1b6f9a
DaxGute <league@WTS14-4.attlocal.net>
2018-07-18 12:53:35 -0700

    ExceptionMethods(ip)

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -5,22 +5,29 @@ public class ExceptionMethods {
 		if (num2 == 0.0) {
 			throw new IllegalArgumentException();
 		}
-		return num1/num2;
+		return num1 / num2;
 	}
+
 	public static String reverseString(String s) {
 		if (s.isEmpty()) {
 			throw new IllegalStateException();
 		}
 		char[] chrArr = s.toCharArray();
 		String[] newArr = new String[chrArr.length];
-		for (int i = chrArr.length-1; i >= 0; i--) {
-			newArr[i] = (""+chrArr[i]);
+		for (int i = chrArr.length - 1; i >= 0; i--) {
+			System.out.println(chrArr[i]);
+			newArr[i] = ("" + chrArr[i]);
 		}
+		System.out.println("\n");
 		String fullString = "";
-		for (int i = 0; i < chrArr.length; i++) {
+
+		for (int i = newArr.length + 1; i > 0 i++) {
 			fullString += newArr[i];
+			System.out.println(fullString);
 		}
+
 		System.out.println(fullString);
+
 		return fullString;
 	}
 }
